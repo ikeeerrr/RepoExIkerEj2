4470aba2a2b21cada7d1a58ba5f3b808c40d9872
Iker Galan <sanchezgalaniker2005@gmail.com>
2024-06-13 09:52:58 +0200

    contenido clase AnalisisEdad

--- src/AnalisisEdad.java
+++ src/AnalisisEdad.java
@@ -1,9 +1,23 @@
-
+import java.util.*;
 public class AnalisisEdad {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public ArrayList <Integer> numeros = new ArrayList<Integer>();
+	int pares = 0;
+	int impares = 0;
+	public void numeros (ArrayList<Integer> numeros) {
+		this.numeros = numeros;
+	}
+	
+	public String comprobar (ArrayList<Integer> numeros) {
+		for (int i=0;i<numeros.get(i);i++) {
+			if(numeros.get(i)%2==0) {
+				pares = pares + numeros.get(i);
+			}
+			if(numeros.get(i)%2==1) {
+				impares = impares + 1;
+			}
+		}
+		String mensaje = "Suma de pares: " + pares + ", Recuento de impares: " + impares ;
+		return mensaje;
 	}
 
 }
