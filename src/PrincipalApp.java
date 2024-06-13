26b53f66f4d4bd9f7936412966c9a719d368f88c
Iker Galan <sanchezgalaniker2005@gmail.com>
2024-06-13 09:56:07 +0200

    quitar el contenido del PrincipalApp en la rama funcionalidadClase

--- src/PrincipalApp.java
+++ src/PrincipalApp.java
@@ -1,20 +1,8 @@
-import java.util.ArrayList;
 
 public class PrincipalApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		ArrayList<Integer> numeros = new ArrayList<Integer>();
-		numeros.add(10);
-		numeros.add(25);
-		numeros.add(12);
-		numeros.add(3);
-		numeros.add(56);
-		numeros.add(78);
-		numeros.add(32);
-		
-		AnalisisEdad analisis = new AnalisisEdad();
-		System.out.println(analisis.comprobar(numeros));
 	}
 
 }
